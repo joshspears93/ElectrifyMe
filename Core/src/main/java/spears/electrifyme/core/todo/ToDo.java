14baf7d73597305aa97668c08f78a34af334b13d
Josh Spears <joshspears93@gmail.com>
2018-05-30 23:18:14 -0400

    remove unused imports

--- Core/src/main/java/spears/electrifyme/core/todo/ToDo.java
+++ Core/src/main/java/spears/electrifyme/core/todo/ToDo.java
@@ -1,19 +1,10 @@
 package spears.electrifyme.core.todo;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.sql.Time;
 import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.Date;
 
 @Data
 @Document(collection = "todo")
